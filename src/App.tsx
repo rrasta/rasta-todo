3b3274dcfb751a95fd41668a9db5b1f97dfde872
Piotr Pliszczyński <fookoo@gmail.com>
2020-10-27 09:25:46 +0100

    New component Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Input from './components/input/input.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Rasta123
+      <Input />
     </div>
-
   );
 }
 
