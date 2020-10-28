7f6a91e63404ea7aa8200a0706ff996432b3908e
Piotr Pliszczyński <fookoo@gmail.com>
2020-10-28 22:09:07 +0100

    Input component JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './components/input/input.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Input />
-    </div>
-  );
+const App = () => {
+    const [todos, setTodos] = useState<string[]>( []);
+    const addNewTodoItem = (todoItem: string) => {
+        if (todoItem === '') {
+            return;
+        }
+
+        setTodos([
+            ...todos,
+            todoItem
+        ]);
+    }
+
+    return (
+        <div className="App">
+            <Input onSubmit={addNewTodoItem} />
+            <ul>
+                {todos.map(todo => (<li key={`todo-${todo}`}>{todo}</li>))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
