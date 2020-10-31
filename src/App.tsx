4394081fe503ea6a7b217107174d84b37e999449
Piotr Pliszczyński <fookoo@gmail.com>
2020-10-31 21:53:38 +0100

    TodoList + TodoItem components. Add checkbox and delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import Input from './components/input/input.component';
+import React from 'react';
+import TodoList from './components/todo-list/todo-list.component';
 import './App.css';
 
 const App = () => {
-    const [todos, setTodos] = useState<string[]>( []);
-    const addNewTodoItem = (todoItem: string) => {
-        if (todoItem === '') {
-            return;
-        }
-
-        setTodos([
-            ...todos,
-            todoItem
-        ]);
-    }
-
     return (
         <div className="App">
-            <Input onSubmit={addNewTodoItem} />
-            <ul>
-                {todos.map(todo => (<li key={`todo-${todo}`}>{todo}</li>))}
-            </ul>
+            <TodoList />
         </div>
     );
 }
---
+++ src/components/todo-list/todo-list.component.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Input from '../input/input.component';
+import TodoItem from '../todo-item/todo-item.component';
+
+import './todo-list.component.scss';
+
+export interface ITodoItem {
+    id: string;
+    text: string;
+    checked: boolean;
+}
+
+// string[] => ['a','b','c']
+// ITodoItem[] => [{ text: 'a', checked: false }, { text: 'b', checked: false } ]
+
+const TodoList = () => {
+    const [todos, setTodos] = useState<ITodoItem[]>( []);
+    const addNewTodoItem = (todoItem: string) => {
+        if (todoItem === '') {
+            return;
+        }
+
+        setTodos([
+            ...todos,
+            {
+                id: `item-${new Date().getTime()}`,
+                text: todoItem,
+                checked: false
+            }
+        ]);
+    }
+    const handleOnChecked = (id: string) => {
+        setTodos(
+            todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        id: todo.id,
+                        text: todo.text,
+                        checked: !todo.checked
+                    }
+                }
+
+                return todo;
+            })
+        )
+    };
+    const handleOnDelete = (id: string) => {
+        // TODO usuwanie elementu z listy
+        // google it: JS array FILTER method
+    };
+
+    return (
+        <div className="todo-list">
+            <Input onSubmit={addNewTodoItem} />
+            <ul>
+                {todos.map(todo => (
+                    <TodoItem
+                        key={todo.id}
+                        todo={todo}
+                        onChecked={handleOnChecked}
+                        onDelete={handleOnDelete}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
